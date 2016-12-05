cb65dfe338f34d8f823111fc207c26308510aa6e
Michael Huang <hyc1124@gmail.com>
2016-12-06 00:41:35 +0800

    utilize the reflection for the response type in callback

--- app/src/main/java/com/example/simplehttpclient/okhttp/HttpCallback.java
+++ app/src/main/java/com/example/simplehttpclient/okhttp/HttpCallback.java
@@ -1,6 +1,10 @@
 package com.example.simplehttpclient.okhttp;
 
+import com.google.gson.internal.$Gson$Types;
+
 import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 import okhttp3.Call;
 
@@ -8,8 +12,24 @@ import okhttp3.Call;
  * Created by huangyanzhen on 2016/12/5.
  */
 
-public interface HttpCallback<T> {
-    void onSuccess(T t);
-    void onFailure(Call call, IOException e);
-    void onError(int statusCode);
+public abstract class HttpCallback<T> {
+
+    public Type mType;
+
+    public HttpCallback() {
+        mType = getSuperClassTypeParameter(this.getClass());
+    }
+
+    private static Type getSuperClassTypeParameter(Class<?> subClass) {
+        Type superClass = subClass.getGenericSuperclass();
+        if (superClass instanceof  Class) {
+            return null;
+        }
+        ParameterizedType parameterizedType = (ParameterizedType) superClass;
+        return $Gson$Types.canonicalize(parameterizedType.getActualTypeArguments()[0]);
+    }
+
+    public void onSuccess(T t) {};
+    public void onFailure(Call call, IOException e) {};
+    public void onError(int statusCode) {};
 }
